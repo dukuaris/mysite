2185366625433f35d2e63b0dfc4c15606ab85b5d
Duke Yoo <dukuaris@gmail.com>
2020-04-24 11:21:50 +0900

    Add Blog App

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,21 +1,23 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# from django.conf import settings
+# from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# from mysite.views import HomeView, UserCreateView, UserCreateDoneTV
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    # shkim
+    # path('accounts/', include('django.contrib.auth.urls')),
+    # # path('accounts/register/', UserCreateView.as_view(), name='register'),
+    # # path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
+    # #
+    # # path('', HomeView.as_view(), name='home'),
+    path('blog/', include('blog.urls')),
+    # path('photo/', include('photo.urls')),
+    # path('visual/', include('visual.urls')),
+    # path('crawl/', include('crawl.urls')),
+    # path('django_plotly_dash/', include('django_plotly_dash.urls')),
+
+] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
